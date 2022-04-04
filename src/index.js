3d553601b26b290411ea903d531ef2ee5ee8c0e2
marqiensis <chervinskaya.alina@gmail.com>
2022-04-04 21:51:20 +0300

    Stage-1 rollback react to 17 version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import styles from './index.module.css';
-import { render } from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import App from './components/App/app.jsx';
 
-function Page () {
-    render(<App />, document.getElementById('root'));
-}
-
-setInterval(Page, 10);
+ReactDOM.render(<App />, document.getElementById('root'));
