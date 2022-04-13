47ab5cd752f815bfb3251b182fde0f744a71e116
marquensis <chervinskaya.alina@gmail.com>
2022-04-13 09:02:29 +0000

    Stage-1 more changes

--- src/fixtures/cart-fixtures.jsx
+++ src/fixtures/cart-fixtures.jsx
@@ -1,28 +1,10 @@
+import cartItemsId from "./cart-item-id";
+import recItems from "./fixtures";
 
-const cartItems = [
-    {
-        "id": "1",
-        "name": "Маска для сна",
-        "color": "Голубой",
-        "weight": "200гр",
-        "image": "/img/1.jpg",
-        "price": "150",
-    },
-    {
-        "id": "2",
-        "name": "Игровая приставка",
-        "color": "Белый",
-        "weight": "3000гр",
-        "image": "/img/2.jpg",
-        "price": "300000",
-    },
-    {
-        "id": "3",
-        "name": "Телефон POCO X3 Pro, новый телефон, флагман, новая модель, смартфон, прикол",
-        "color": "Розовая жемчужина",
-        "weight": "500гр",
-        "image": "/img/3.jpg",
-        "price": "33000",
-    },
-]
+const cartItems = cartItemsId.map((item) => {
+    const id = (rec) => item.id === rec.id;
+    const equalId = recItems.find(id);
+    return Object.assign({}, equalId, item);
+});
+
 export default cartItems;
---
+++ src/fixtures/cart-item-id.jsx
@@ -0,0 +1,15 @@
+const cartItemsId = [
+    {
+        "id": "1",
+        "count" : 1,
+    },
+    {
+        "id": "2",
+        "count" : 2,
+    },
+    {
+        "id": "3",
+        "count" : 3,
+    },
+]
+export default cartItemsId;
