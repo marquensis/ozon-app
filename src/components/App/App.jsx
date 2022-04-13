daf5fa8f7dbc7f97fa4ec75053e0dec0a6c36a44
marquensis <chervinskaya.alina@gmail.com>
2022-04-13 10:01:31 +0000

    Stage-1 changed cartItems func

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import AppHeader from "../AppHeader/AppHeader";
 import RecommendedList from "../Recommended/Recommended";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
-import cartItems from "../../fixtures/cart-fixtures";
+import cartItemsId from "../../fixtures/cart-item-id";
 import recItems from "../../fixtures/fixtures";
 import "swiper/css/bundle";
+
+const cartItems = cartItemsId.map((item) => {
+    const equalId = recItems.find(rec => rec.id === item.id);
+    return { ...item, ...equalId };
+})
 
 function App () {
     return (
