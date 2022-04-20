273ba6501008d1beea4056955a2c88c0130aa8f7
marquensis <chervinskaya.alina@gmail.com>
2022-04-20 14:55:54 +0300

    Stage-1 PropTypes errors

--- src/shapes/CartShapes.jsx
+++ src/shapes/CartShapes.jsx
@@ -1,12 +1,12 @@
 
 import PropTypes from 'prop-types';
 
-const CartShapes = {
+const CartShapes = PropTypes.shape({
     name: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     weight: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-}
+});
 
 export default CartShapes;
--- src/shapes/RecShapes.jsx
+++ src/shapes/RecShapes.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-const RecommendedShapes = {
+const RecommendedShapes = PropTypes.shape({
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
-}
+});
 
 export default RecommendedShapes;
