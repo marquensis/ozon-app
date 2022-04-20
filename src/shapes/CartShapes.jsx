6952c124999dc83bc0cebe0f41e1f17e04afdd82
marquensis <chervinskaya.alina@gmail.com>
2022-04-20 20:47:02 +0300

    Stage-1 more shapes

--- src/shapes/CartShapes.jsx
+++ src/shapes/CartShapes.jsx
@@ -1,7 +1,7 @@
 
 import PropTypes from 'prop-types';
 
-const CartShapes = ({
+const CartShapes = PropTypes.shape({
     name: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     weight: PropTypes.string.isRequired,
--- src/shapes/RecShapes.jsx
+++ src/shapes/RecShapes.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const RecommendedShapes = ({
+const RecommendedShapes = PropTypes.shape({
     name: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
